540094b3658fd00cb396d719c30ea39b03ebd34b
fischerc <fischerc@hu-berlin.de>
2020-04-28 18:25:03 +0200

    rm print command

--- skreins.py
+++ skreins.py
@@ -64,7 +64,6 @@ for excel in excel_files:
 
     
 #bring edited 'clean' column back to origine
-    #print("cleaned ", cleaned)
     with open(cleaned) as f: 
         reader = csv.reader(f, delimiter=';')
         for i, row in enumerate(reader):
