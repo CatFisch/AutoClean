6a2774a6a96cef628511d498d4ce879e53628010
fischerc <fischerc@hu-berlin.de>
2020-04-28 17:47:22 +0200

    add Alex code

--- skreins.py
+++ skreins.py
@@ -5,57 +5,89 @@ import os
 import pandas 
 import subprocess
 import sys
-#import xlsxwriter
 import openpyxl
 import csv
+from itertools import chain
+#import xlsxwriter
+
+import clean_skript_V3
+
 
 #convert lb and clean column to .txt
 excel_files = glob.glob(os.getcwd() + '/*.xlsx') 
 
 for excel in excel_files:
     base_name = excel.split('.')[0]
-    convex = base_name + '_CONV.txt'
+    convex = base_name + '_CONVED.txt'
 #apply clean-skript_V3.py
-    df = pandas.read_excel(excel) 
+    df = pandas.read_excel(excel)
     df[['lb','dipl']].to_csv(convex, index=False, sep='\t') 
-    output = convex[:-9] + '_CLEANED.csv'
-    subprocess.call(['python3', 'clean-skript_V3.py', convex, output])
-#extract cleaned 'dipl' column as string 
-    cleaned = base_name + '_CLEANDIPL.csv' 
-    col = pandas.read_csv(output, error_bad_lines=False, sep='\t') 
-    cleaned_col = col[['dipl']].to_csv(cleaned, index = None, header=True)
-#1)bring edited 'clean' column back to origine
-    wb = openpyxl.load_workbook(excel)
+    output = base_name + '_CLEANED.csv'
+    #subprocess.call(['python3', 'clean-skript_V3.py', convex, output], shell=True
+    clean_skript_V3.main(["", convex, output])
+#rename cleaned 'dipl' column as string 
+    cleaned = base_name + '_CLEANDIPLED.csv'
+    col = pandas.read_csv(output, error_bad_lines=False, sep='\t')
+    col_rename = col.rename(columns={'dipl':'clean'}, index = None)
+    cleaned_col = col_rename[['clean']].to_csv(cleaned, index = None, header=True)
+    origclean = pandas.read_csv(cleaned, error_bad_lines=False, sep='\t')
+    zz = base_name + '_FINAL.xlsx'
+    ben = origclean.to_excel(zz, index = None)
+    uu = pandas.read_excel(zz)
+    print(uu)
+#insert new 'clean' column (delete old one)
+    wb = openpyxl.load_workbook(filename=excel, read_only=False)
     ws = wb.active
     dipl_col_index = None
     clean_col_index = None
-
+    
     for e, col in enumerate(ws.iter_cols()):     
         if col[0].value == 'clean':
-            clean_col_index = e+1
+           clean_col_index = e+1
     if clean_col_index is not None:
         ws.delete_cols(clean_col_index)
     for e, col in enumerate(ws.iter_cols()):
         if col[0].value == 'dipl':
             dipl_col_index = e+1
-    print('dipl_col_index=', dipl_col_index)
     ws.insert_cols(dipl_col_index+1)
-    #find merged cells
-    for r in ws.merged_cells.ranges:
-        #find unmerged cells
-        ws.unmerge_cells(r)
+
+#need to save and reopen,
+#otherwise new inserted column won't be found when deleting merged cells
+    wb.save("TEMP.xlsx")   
+    wb = openpyxl.load_workbook(filename="TEMP.xlsx", read_only=False)
+    ws = wb.active
+    
+#find and unmerge merged cells
+    merged_cells = list(ws.merged_cells.ranges)
+    for cr in merged_cells:    
+        if(cr.min_col==dipl_col_index+1): 
+            ws.unmerge_cells(range_string=str(cr))
 
     
-    with open(cleaned) as f:
+#bring edited 'clean' column back to origine
+    print("cleaned ", cleaned)
+    with open(cleaned) as f: 
         reader = csv.reader(f, delimiter=';')
         for i, row in enumerate(reader):
-                for j, cell in enumerate(row): 
-                    ws.cell(row=i+1, column=dipl_col_index+1).value = 'ben'
-                    
+                c = ws.cell(row=i+1, column=dipl_col_index+1)
+                c.value = row[0]
 
-    wb.save('output.xlsx') #wb.save(basename + '.xlsx') 
-    
-   # wb.save(output[:-5] + '.xlsx') #hier setzte ich später base_name statt output[... ein
+    last_value = -1
+    merging = False
+    for row in ws.iter_rows(min_col=dipl_col_index+1, max_col=dipl_col_index+1): 
+        cell = row[0]
+        #print("row ", row.index, "  value ", cell.value)
+        if not cell.value:
+            if not merging:
+                merging = True
+                last_value = cell.row-1
+
+        elif merging:
+            ws.merge_cells(start_row=last_value, start_column=dipl_col_index+1, end_row=cell.row-1, end_column=dipl_col_index+1)
+            merging = False
 
 
-    
+    wb.save("FINAL.xlsx")
+
+
+ 
